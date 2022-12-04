da5a9f11f6afac66e10b1ead2cd4588317ce2a66
thetechgirlgita <thetechgirlgita@gmail.com>
2022-12-04 12:43:46 +0530

    Clearing screen from starch

---
+++ lib/clock_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class clock extends StatefulWidget {
+  const clock({Key? key}) : super(key: key);
+
+  @override
+  _clockState createState() => _clockState();
+}
+
+class _clockState extends State<clock> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+    );
+  }
+}
